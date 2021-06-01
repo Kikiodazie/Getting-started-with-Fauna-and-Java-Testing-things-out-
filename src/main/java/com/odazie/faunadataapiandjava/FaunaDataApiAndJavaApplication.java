83c968399ef4dda4983d09bc15dc0859287a844a
Kikiodazie <kiki.odazie@yahoo.com>
2021-06-01 15:45:10 +0100

    Initial commit

---
+++ src/main/java/com/odazie/faunadataapiandjava/FaunaDataApiAndJavaApplication.java
@@ -0,0 +1,13 @@
+package com.odazie.faunadataapiandjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaunaDataApiAndJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaunaDataApiAndJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/odazie/faunadataapiandjava/FaunaDataApiAndJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odazie.faunadataapiandjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaunaDataApiAndJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
